a9b2f84742f167a3a2ae2871da540e95043f5203
Oecke <oeckerone@gmail.com>
2025-06-08 22:37:56 +0300

    comment

--- Login/login.js
+++ Login/login.js
@@ -1,62 +1,29 @@
-function App() {
-  const [users, setUsers] = React.useState(() => {
-    return JSON.parse(localStorage.getItem('users')) || [];
-  });
+const App = () => {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
-  const [confirmPassword, setConfirmPassword] = React.useState('');
-  const [errorMessage, setErrorMessage] = React.useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      setErrorMessage('Passwords do not match');
-      return;
-    }
-    if (users.some(user => user.username === username)) {
-      setErrorMessage('Username already exists');
-      return;
-    }
-    const newUser = { username, password };
-    const updatedUsers = [...users, newUser];
-    setUsers(updatedUsers);
-    localStorage.setItem('users', JSON.stringify(updatedUsers));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('login_username', username);
+    localStorage.setItem('login_password', password);
     setUsername('');
     setPassword('');
-    setConfirmPassword('');
-    setErrorMessage('');
+    alert('Login data saved!');
   };
 
   return (
     <div>
-      <h2>Add User</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} required />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} required />
-        </div>
-        <div>
-          <label>Confirm Password:</label>
-          <input type="password" name="confirmPassword" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} required />
-        </div>
-        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-        <button type="submit">Add User</button>
+      <form className="login__form" onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <input type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} required />
+        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} required />
+        <button type="submit">Login</button>
       </form>
-      <h2>Users List</h2>
-      <ul>
-        {users.map((user, index) => (
-          <li key={index}>{user.username}</li>
-        ))}
-      </ul>
     </div>
   );
 }
 
 ReactDOM.render(
-  <App />,
+  <App />, 
   document.querySelector('body')
 );
